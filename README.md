89552d49fb875634be3cce5d8b5f1af765368a02
bkumar2 <bipul_kumar@intuit.com>
2016-03-13 10:57:50 +0530

    First commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# datetime-ext (JavaScript Datetime Extension)
+
+datetime-ext is a Node.js module that has utility functions to parse date, datetime, time etc.
+
+## Installation
+
+```
+npm install datetime-ext --save
+```
+
+## Usage
+
+```javascript
+var datetimeExt = require('datetime-ext');
+```
+
+## API Documentation
---
+++ index.js
@@ -0,0 +1,54 @@
+/*!
+ * datetime-ext
+ * https://github.com/bkumar2/datetime-ext.git
+ *
+ * Copyright 2014 Bipul Kumar
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+var datetimeExt = {
+};
+
+module.exports = datetimeExt;
+
+function _parse(datetime,datetimeFormat) {
+	console.log("datetime:",datetime,"datetimeFormat",datetimeFormat);
+	var tokens = [/d+/,/M+/,/y+/,/h+/,/m+/,/s+/];
+	var tokenIndexArray = new Array();
+	for(var i=0; i<tokens.length; ++i) {
+		tokenIndexArray[i] = datetimeFormat.search(tokens[i]);
+	}
+	var sortedIndexArray = tokenIndexArray.slice().sort(function(a, b){return a-b});
+	console.log("tokenIndexArray:",tokenIndexArray);
+	console.log("regexIndexArray:",sortedIndexArray);
+	var regexIndexArray = new Array();
+	for(var i=0; i<sortedIndexArray.length; ++i) {
+		regexIndexArray[i] = tokenIndexArray.indexOf(sortedIndexArray[i])+1;
+	}
+	console.log("regexIndexArray:",regexIndexArray);
+	var datetimeRegexPattern = datetimeFormat;
+	var outputPattern = datetimeFormat;
+	for(var i=0; i<tokens.length; ++i) {
+		var matchedToken = datetimeRegexPattern.match(tokens[i]);
+		datetimeRegexPattern = datetimeRegexPattern.replace(matchedToken,"(\\d{"+matchedToken[0].length+"})");
+	}
+	var datetimeRegex = new RegExp(datetimeRegexPattern);
+	var outputDatetimeString = datetime.replace(datetimeRegex,"$2/$3/$1 $4:$5:$6")
+	console.log("outputDatetimeString:",outputDatetimeString);
+	var outputDatetime = new Date(outputDatetimeString);
+	console.log("datetimeRegexPattern:",datetimeRegexPattern);
+	console.log("outputDatetime:",outputDatetime);
+}
+
+_parse("2009-07-12 12:34:56","yyyy-MM-dd hh:mm:ss");
